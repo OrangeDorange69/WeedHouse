f6c086819e91ffa0ba3d260f0f45a0c1d2db14cf
OrangeDorange69 <135204579+OrangeDorange69@users.noreply.github.com>
2024-02-28 09:12:00 +0100

    22

--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
-function getUserInput(promptMessage) {
-    return window.prompt(promptMessage).trim();
+function showAlert(message) {
+    const outputElement = document.getElementById('nameofUser');
+    outputElement.textContent = message;
 }
 
-function showAlert(message) {
-    window.alert(message);
+function getUserInput(promptMessage, defaultValue = '') {
+    const userInput = prompt(promptMessage, defaultValue);
+    return userInput ? userInput.trim() : '';
 }
 
 function generateVerificationCode() {
@@ -11,7 +13,7 @@ function generateVerificationCode() {
 }
 
 function verifyCode(code) {
-    return window.prompt(`Proveď ověření zadáním následujícího čísla: ${code}`);
+    return prompt(`Proveď ověření zadáním následujícího čísla: ${code}`);
 }
 
 function calculateDaysAlive(birthDate) {
@@ -33,13 +35,10 @@ function generateRandomWord() {
     const consonants = "bcčdďfghjklmnňpqrřsštťvwxzž";
     const vowels = "aeiouyáéěíóúý";
 
-    const getRandomConsonant = () => getRandomElement(consonants.split(''));
-    const getRandomVowel = () => getRandomElement(vowels.split(''));
-
     let wordLength;
 
     do {
-        const userInput = prompt("Představuji generátor náhodných rap slov, napiš jak dlouhé má být tvé slovo:");
+        const userInput = getUserInput("Představuji generátor náhodných rap slov, napiš jak dlouhé má být tvé slovo:");
 
         if (userInput === null) {
             return;
@@ -52,7 +51,7 @@ function generateRandomWord() {
     let randomWord = "";
 
     for (let i = 0; i < wordLength; i++) {
-        randomWord += i % 2 === 0 ? getRandomConsonant() : getRandomVowel();
+        randomWord += i % 2 === 0 ? getRandomElement(consonants.split('')) : getRandomElement(vowels.split(''));
     }
 
     showAlert(`Generátor náhodných rap slov vybral slovo: ${randomWord}`);
@@ -186,6 +185,5 @@ const imgElement = document.querySelector('img[src="./cookie.png"]');
 if (imgElement) {
   imgElement.src = 'https://www.shutterstock.com/image-vector/happy-smiling-kawaii-cute-cookie-600nw-2315585199.jpg';
 }
-
 
 console.log(`Rare jako supreme drop`);
